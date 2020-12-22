892d7bb30efad84f3048f863c85c9840984c3737
Gregor Feierabend <23300469@sun.ac.za>
2020-12-22 11:03:41 +0200

    changes in DBG preprocessor directive

--- proveparser.c
+++ proveparser.c
@@ -10,18 +10,16 @@
 #define DPARSER
 
 #ifdef DPARSER
-#define DBG(enable, msg) \
-	if (!quiet && ((output_lvl == -1 && enable) || output_lvl == lvl)) \
-	printf("%s", msg);
+#define DBG(cmd) \
+	if (!quiet) { cmd }
 #else
-#define DBG(msg)
+#define DBG(cmd)
 #endif
 
 Token    token;                     /* current token					*/
 Pnode*   pnode;                     /* current node in graph			*/
 FILE*    file;                      /* [prove] source file				*/
 static unsigned short int lvl;      /* indentation level				*/
-static short int output_lvl;		/* print, when on this level		*/
 
 void parse_expr(void);
 int parse_formula(void);
@@ -32,8 +30,8 @@ void check_conflict(Pnode* pnode, TType ttype);
 
 int main(int argc, char *argv[])
 {
-	if (argc < 2 && argc > 4) {
-		fprintf(stderr, "usage: %s <filename> [<output_level>] [quiet]\n",
+	if (argc < 2) {
+		fprintf(stderr, "usage: %s <filename> [quiet]\n",
 				argv[0]);
 		exit(EXIT_FAILURE);
 	} else if ((file = fopen(argv[1], "r")) == NULL) {
@@ -46,14 +44,8 @@ int main(int argc, char *argv[])
 	init_pgraph(&pnode);
 	next_token(&token);
 
-	if (argc == 4) {
+	if (argc > 2) {
 		quiet = TRUE;
-	}
-
-	if (argc == 3) {
-		output_lvl = atoi(argv[2]);
-	} else {
-		output_lvl = -1;
 	}
 
 	/* <expr>
@@ -83,23 +75,21 @@ int parse_formula(void)
 	/* TODO perform some check for ERRORS (wrt to EQ and IMP positioning */
 	for (int proceed = TRUE; proceed;) {
 		if (IS_FORMULATOR(token.type)) {
-			DBG(TRUE, token.id)
+			DBG(printf("%s", token.id);)
 			set_symbol(pnode, token.id);
 			check_conflict(pnode, token.type);
 
 			next_token(&token);
-			DBG(FALSE, ".")
 			parse_statement();
 			if (!IS_FORMULATOR(token.type)) {
 				proceed = FALSE;
 			}
 		} else {
-			DBG(FALSE, ".")
 			parse_statement();
 			if (!IS_FORMULATOR(token.type)) {
 				return FALSE;
 			} else {
-				DBG(TRUE, token.id)
+				DBG(printf("%s", token.id);)
 
 				create_right(pnode);
 				move_right(&pnode);
@@ -124,7 +114,7 @@ void parse_statement(void)
 
 	for (int proceed = TRUE; proceed;) {
 		lvl++;
-		DBG(TRUE, token.id)
+		DBG(printf("%s", token.id);)
 		expect(TOK_LBRACK);
 
 		if (HAS_CHILD(pnode) || HAS_SYMBOL(pnode)) {
@@ -139,11 +129,11 @@ void parse_statement(void)
 			next_token(&token);
 			if (token.type == TOK_RBRACK) {
 				/* token is an identifier */
-				DBG(TRUE, *(pnode->symbol))
+				DBG(printf("%s", *(pnode->symbol));)
 			} else if (token.type == TOK_LBRACK) {
 				/* token is a formulator */
 				/* check for conflicting flags and report ERROR */
-				DBG(TRUE, *(pnode->symbol))
+				DBG(printf("%s", *(pnode->symbol));)
 				SET_NFLAG_FMLA(pnode)
 				parse_expr();
 			} else {
@@ -151,7 +141,7 @@ void parse_statement(void)
 				/* ERROR */
 			}
 		} else if (token.type == TOK_IMPLY) {
-			DBG(TRUE, token.id)
+			DBG(printf("%s", token.id);)
 			/* token is an implication symbol */
 			next_token(&token);
 			if (token.type == TOK_RBRACK) {
@@ -166,7 +156,7 @@ void parse_statement(void)
 				/* ERROR */
 			}
 		} else if (token.type == TOK_EQ) {
-			DBG(TRUE, token.id)
+			DBG(printf("%s", token.id);)
 			/* statements must not begin with an equality token */
 			/* ERROR */
 		} else if (token.type == TOK_LBRACK) {
@@ -178,7 +168,7 @@ void parse_statement(void)
 			/* ERROR */
 		}
 
-		DBG(TRUE, token.id)
+		DBG(printf("%s", token.id);)
 		expect(TOK_RBRACK);
 		move_and_sum_up(&pnode);
 
@@ -212,16 +202,14 @@ void parse_statement(void)
 
 		if (HAS_NFLAG_IMPL(pnode) && !HAS_NFLAG_ASMP(pnode)) {
 			init_reachable(pnode);
-			printf("{%d}", pnode->num);
+			DBG(printf("{%d}", pnode->num);)
 			while (next_reachable_const(pnode)) {
-				printf("<%d", rn());
+				DBG(printf("<%d", rn());)
 				if(same_as_rchbl(pnode)) {
 					/* TODO skip unneccessary cmps */
-					/* TODO remember to exit_branch */
-					//printf("(%d)", rn()); /* DEBUG "reason" */
-					printf("#");
+					DBG(printf("#");)
 				}
-				printf(">");
+				DBG(printf(">");)
 			}
 		}
 
@@ -260,6 +248,13 @@ void expect(TType type)
 void check_conflict(Pnode* pnode, TType ttype)
 {
 	if (ttype == TOK_IMPLY) {
+		DBG(if (!HAS_NFLAG_ASMP(pnode)) {
+			printf("\n");
+			for (int i = 0; i < lvl; i++ ) {
+				printf("\t");
+			}
+		})
+
 		if (!HAS_FFLAGS(pnode)) {
 			SET_NFLAG_IMPL(pnode)
 		} else if (HAS_NFLAG_IMPL(pnode)) {
